30de72d50580fddaaf4615739eab95df25d3bfeb
Barry Stump <weevilgenius@users.noreply.github.com>
2026-09-04 10:23:39 -0700

    vitest: fix deprecation warning

--- vitest.config.ts
+++ vitest.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   resolve: {
     alias: {
       // Allow imports like 'src/types' in test files
-      src: path.resolve(__dirname, './src'),
+      src: path.resolve(import.meta.dirname, './src'),
     },
   },
 });
